a5c34cb634c5dcae0354d2dc50ebc7d08cb6da37
jeevesh <jeevesh2802@gmail.com>
2025-04-29 23:00:03 +0530

    server branch experiment

--- Frontend/src/lib/utils.js
+++
@@ -1,7 +0,0 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-// Utility function for combining Tailwind classes
-export function cn(...inputs) {
-  return twMerge(clsx(inputs));
-}
